df61e3650caff5153d47fc38d627561fd896765f
Prajwal Kuchewar <prajwalkuchewar3@gmail.com>
2024-10-01 15:37:29 +0530

    intial commit

--- quiz-service/quiz-service/src/main/java/com/prajwal/quiz_service/repository/QuizRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prajwal.quiz_service.repository;
-
-import com.prajwal.quiz_service.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizRepository extends JpaRepository<Quiz, Integer> {
-}
